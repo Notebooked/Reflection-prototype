879f08464f28391725136dd7ea605c4b4e451ca2
juicetanz <66227184+juicetanz@users.noreply.github.com>
2023-04-07 10:03:19 -0700

    fix bullet i swear

--- Bullet.gd
+++ Bullet.gd
@@ -1,7 +1,7 @@
 extends KinematicBody2D
 
-var velocity = Vector2(0, 0)
+var velocity = Vector2(0, 1)
 var speed = 300
 
 func _physics_process(delta):
-	var collision_info = move_and_collide(velocity.normalized() * delta * speed)
+	var collision_info = move_and_collide(velocity.normalized() * speed)
