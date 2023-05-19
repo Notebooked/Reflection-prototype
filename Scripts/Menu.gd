10dd1e754e35db25995f6a198ab039533c0196d8
juicetanz <66227184+juicetanz@users.noreply.github.com>
2023-05-18 19:34:02 -0700

    adding scenes

--- Scripts/Menu.gd
+++ Scripts/Menu.gd
@@ -1,7 +1,7 @@
 extends Control
 
 func _on_PlayButton_pressed():
-	get_tree().change_scene_to_file("res://Main.tscn")
+	get_tree().change_scene_to_file("res://Scenes/Main.tscn")
 
 func _on_QuitButton_pressed():
 	get_tree().quit()
