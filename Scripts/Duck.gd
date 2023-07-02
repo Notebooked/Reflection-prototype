c0ca66edc03cc60227b525b9b0c47a0ecffeb735
juicetanz <66227184+juicetanz@users.noreply.github.com>
2023-07-01 20:55:37 -0700

    yippee

--- Scripts/Duck.gd
+++ Scripts/Duck.gd
@@ -1,10 +1,10 @@
 extends Area2D
 
-var speed = 1
+var speed = 60
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	var collision = $Area2D.get_overlapping_bodies()
 	for object in collision:
 		if object.name == "Player":
-			position.x += (object.position.x - position.x) / 100
+			position.x += (object.position.x - position.x).normalized() * delta * speed
