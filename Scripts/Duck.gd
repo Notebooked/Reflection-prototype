ca48021f222b754bc24f5c7a7232b6ba1a4f3e23
juicetanz <66227184+juicetanz@users.noreply.github.com>
2023-07-10 20:03:08 -0700

    attack

--- Scripts/Duck.gd
+++ Scripts/Duck.gd
@@ -1,19 +1,31 @@
 extends Area2D
 
 var speed = 120
-#var state = "idle"
+var timer = 0
+var endTime = 3
+var attacking = false
+
+func attack():
+	attacking = false
+	
+	return
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	var collision = get_parent().get_overlapping_bodies()
-	for object in collision:
-		if object.name == "Player":
-			#state = "move"
-			
-			var temp = object.global_position - global_position
-			temp.y = 0
-			
-			#if temp.normalized().x <= 5:
-				#state = "attack"
-			
-			position.x += temp.normalized().x * delta * speed
+	if attacking:
+		timer += delta
+		if timer >= endTime:
+			attack()
+	else:
+		var collision = get_parent().get_overlapping_bodies()
+		for object in collision:
+			if object.name == "Player":
+				var temp = object.global_position - global_position
+				temp.y = 0
+				
+				if abs(temp.x) <= 5:
+					print(temp)
+					attacking = true
+				else:
+					print("move")
+					position.x += temp.normalized().x * delta * speed
